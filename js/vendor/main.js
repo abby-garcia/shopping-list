2436121485cc2e946bf2c7d15de8e108fd4497bf
abby-garcia <acg361@gmail.com>
2016-02-23 14:01:30 -0500

    clear button

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 	$('#input-box').on('keypress', function(e) {
 	if(e.keyCode == 13){
 		var foodType = $(this).val(); //grabs value of input
-		var foodContent = '<li> <i class="fa fa-minus fa-2x"></i>' + foodType + '<i class="fa fa-check fa-2x"></i></li>'
+		var foodContent = '<li> <i class="fa fa-minus fa-2x"></i>' + foodType + '<i class="fa fa-check fa-2x"></i></li>';
 		$('.shopping-list ul').prepend(foodContent);
 		$(this).val(""); // put this here, so it reverts back to empty string
 	}
@@ -18,20 +18,33 @@ $( document ).ready(function() {
 
 // Minus sign - when pressed deletes the entry
 
-	$('li').on("click", '.fa-minus',function (){
+	$('ul').on("click", '.fa-minus',function (){
 		$(this).parent().remove();
-		
 	});
 
 // Check sign - when pressed, makes a line across the item 
 
-	$('li').on("click",'.fa-check',function (){
-		$(this).parent().css("text-decoration", "line-through");
-		
+	$('ul').on("click",'.fa-check',function (){
+		$(this).parent().toggleClass('underline');		
 	});
 
 
 
+// Reset Button
+
+	$('#clear-button').on("click",function (){
+		$('li').remove('underline');
+
+		//this needs to remove words with 
+		//lines thru them
+
+	});
+
+
 }); //belongs to line 1 
 
-// the minus and check marks aren't working on the new things that have been added
+
+
+//Challeneg 1. If the person clicks on the check mark again,
+// it can uncheck
+// 2. A Clear Button that removes everyting that is checked
